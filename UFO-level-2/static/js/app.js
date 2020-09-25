c6187ab141c9122356b728854edd3fc093362638
jbstogner <stognerbrian@gmail.com>
2020-09-24 22:26:04 -0400

    complete

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,14 +1,13 @@
 // from data.js
 var result = data;
+var tbody = d3.select("tbody");
 
-// Function to add populate the html table with data data.js file
+// Function to populate the html table with data from the data.js file
 function appendRow(rowEntry) {
-    var tbody = d3.select("tbody");
     var row = tbody.append("tr");
     Object.entries(rowEntry).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
-        // console.log(cell);
     });
 };
 
@@ -22,39 +21,35 @@ function refreshPage() {
 
 // Select the button, form, and inputs
 var button = d3.select("#reset-btn");
-// var form = d3.select("form");
 var inputs = d3.selectAll('input');
 
 // Create event handlers for clicking the button or entering data into a filter
 button.on("click", refreshPage);
-// form.on("submit", runEnter);
 inputs.on("change", runEnter);
+
+var filteredData;
 
 // Create the function to run for both events
 function runEnter() {
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-  
-    // Get the key & value property of the input element
-    // Convert elements to lower case (to match the table data)
-    var key = d3.select(this).property('id');
-    var value = d3.select(this).property('value').toLowerCase();
+    filteredData = data;
 
-    result = result.filter(obj => obj[key] == value);
-    
-    // Remove all table rows
-    // Create a function to delete all rows other than the first
-    function deleteRows() {
-        var rowCount = ufo_table.rows.length;
-        for (var i = rowCount -1; i > 0; i--) {
-            ufo_table.deleteRow(i);
-        }
-    };
-    // Call the deleteRows function
-    deleteRows()
+    // Apply filters to the data
+    inputs._groups[0].forEach(val => {
+        if(val.value){
+            filteredData = filteredData.filter(obj => obj[val.id] == val.value.toLowerCase());
+        };
+    });
+
+    // Clear the table
+    tbody.html('');
 
     // Populate the rows with data that fit the filter criteria
-    result.forEach(appendRow);
+    filteredData.forEach(appendRow);
+
+    if(filteredData.length == 0) {
+        window.alert("No results match the selected filter criteria.")
+        refreshPage();
+    }
 
 };
